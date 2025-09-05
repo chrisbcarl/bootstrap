8905f9b1130a59bcd85602a70c155cb3a8bdcb42
Chris Carl <chrisbcarl@outlook.com>
2025-09-04 21:13:23 -0700

    careful re-read what id merged

--- operating-systems/posix/ubuntu-on-new-pc.sh
+++ operating-systems/posix/ubuntu-on-new-pc.sh
@@ -1,28 +1,25 @@
 sudo apt update -y
 sudo apt upgrade -y
 
+
 # server management
 sudo apt install ssh -y  # i've seen machines NOT come with this installed / enabled out of the box
 sudo apt install tmux htop nvtop -y
 
+
 # benchmarkers
 sudo apt install stress -y
-
-# languages
-
-# tools
-sudo apt install dos2unix -y
 
 
 # devtools / languages
 sudo apt install build-essential linux-headers-$(uname -r) -y
 sudo apt install git -y
-git config --global user.email "chrisbcarl@outlook.com"
-git config --global user.name "Chris Carl"
+sudo apt install dos2unix -y
 sudo apt install default-jdk -y
 sudo apt install -y gcc g++ make cmake gdb valgrind
 sudo apt install python3 -y
-sudo ln -s $(which python3) /usr/bin/python -f
+sudo apt install python3.10 -y
+sudo ln -s $(which python310) /usr/bin/python -f
 sudo apt install vim -y
 
 
