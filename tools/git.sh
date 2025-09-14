ceab27bdbe885826367c4ad08b28862f37e931c3
Chris Carl <chrisbcarl@outlook.com>
2025-09-13 18:14:20 -0700

    b into a

--- tools/git.sh
+++ tools/git.sh
@@ -6,15 +6,13 @@ git config --global pull.rebase false  # makes merge the default rather than reb
 git config --global core.autocrlf false  # set everything to LF rather than CRLF
 
 
-# merge 2 repositories - https://stackoverflow.com/a/10548919
+# merge 2 repositories - merge project b INTO project a - (modified from) https://stackoverflow.com/a/10548919
 cd path/to/project-a
-git checkout some-branch
-
-cd path/to/project-b
-git remote add project-a /path/to/project-a
-git fetch project-a --tags
-git merge --allow-unrelated-histories project-a/some-branch
-git remote remove project-a
+git checkout main
+git remote add project-b /path/to/project-b
+git fetch project-b --tags
+git merge --allow-unrelated-histories project-b/main
+git remote remove project-b
 
 
 # does it for the whole branch...
