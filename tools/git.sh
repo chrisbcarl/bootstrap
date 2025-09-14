cbc3f0ba03d6d42e452fc108d954b1082de6e924
Chris Carl <chrisbcarl@outlook.com>
2025-09-13 18:05:41 -0700

    merge 2 projects

--- tools/git.sh
+++ tools/git.sh
@@ -6,6 +6,16 @@ git config --global pull.rebase false  # makes merge the default rather than reb
 git config --global core.autocrlf false  # set everything to LF rather than CRLF
 
 
+# merge 2 repositories - https://stackoverflow.com/a/10548919
+cd path/to/project-a
+git checkout some-branch
+
+cd path/to/project-b
+git remote add project-a /path/to/project-a
+git fetch project-a --tags
+git merge --allow-unrelated-histories project-a/some-branch
+git remote remove project-a
+
 
 # does it for the whole branch...
 # https://github.com/newren/git-filter-repo/blob/main/INSTALL.md
