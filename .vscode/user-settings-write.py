6ff5dd7807ba7f3fae57fdd430ac5ce8dd821161
Chris Carl <chrisbcarl@outlook.com>
2025-11-28 01:22:26 -0800

    more vscode shit

--- .vscode/user-settings-write.py
+++ .vscode/user-settings-write.py
@@ -21,15 +21,19 @@ import shutil
 # C:\Users\chris\AppData\Roaming\Code\User\settings.json
 if sys.platform == 'win32':
     user_settings_json = os.path.abspath(os.path.expanduser("~/AppData/Roaming/Code/User/settings.json"))
+    user_keyboard_json = os.path.abspath(os.path.expanduser("~/AppData/Roaming/Code/User/keybindings.json"))
 else:
     raise NotImplementedError(sys.platform)
 
 bootstrap_settings_json = os.path.join(os.path.dirname(__file__), 'settings.json')
+bootstrap_keybindings_json = os.path.join(os.path.dirname(__file__), 'settings.json')
 
 # copy bootstrap settings to user path
-shutil.copy2(bootstrap_settings_json, user_settings_json)
+print("copied to", shutil.copy2(bootstrap_settings_json, user_settings_json))
+print("copied to", shutil.copy2(bootstrap_keybindings_json, bootstrap_settings_json))
 
 # read the bootstrap/settings.json by clearing out all of the comments
+print("modifying the user vscode settings after copy")
 with open(bootstrap_settings_json, 'r', encoding='utf-8') as r:
     bootstrap_content = r.read()
 
@@ -53,7 +57,7 @@ keys_value_to_replace = [
 for key, value in keys_value_to_replace:
     bootstrap_value = json.dumps(bootstrap_data[key])
     user_replace_value = json.dumps(value)
-    print(f'setting user setting "{key}" to "{user_replace_value}"')
+    print(f'    setting user setting "{key}" to "{user_replace_value}"')
     user_content = user_content.replace(bootstrap_value, user_replace_value)
 
 # print(user_content)
