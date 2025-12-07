dd07ee6215881bbd0800cc111fec047eadd81d54
Chris Carl <chrisbcarl@outlook.com>
2025-12-06 20:00:16 -0800

    covered every base regex-wise

--- languages/python/regex.py
+++ languages/python/regex.py
@@ -52,6 +52,13 @@ for match_object in re.finditer(r'\d{3,}', body):
     exactly_3 = match_object.group()
     print(exactly_3)
 
-('\n\n\nex) 7: replace groups')
+print('\n\n\nex) 7: replace groups')
 result = re.sub(r"\(?(\d{3})\)?[ -]?(\d{3})-?(\d{4})", r"\1\2\3", body)
+print(result)
+
+# takes 2 regexes
+print('\n\n\nex) 8: remove HTML tag attributes')  # <p _data-start="hello_world:0.0; 0.1-0.3,0.4">
+html = '''<path fill-rule="evenodd" clip-rule="evenodd" d="M10.3333 2.08496C14.7046 2.08496 18.2483 5.62867 18.2483 10C18.2483 14.3713 14.7046 17.915 10.3333 17.915C5.96192 17.915 2.41821 14.3713 2.41821 10C2.41821 5.62867 5.96192 2.08496 10.3333 2.08496ZM10.3333 3.41504C6.69646 3.41504 3.74829 6.3632 3.74829 10C3.74829 13.6368 6.69646 16.585 10.3333 16.585C13.97 16.585 16.9182 13.6368 16.9182 10C16.9182 6.3632 13.97 3.41504 10.3333 3.41504Z"></path></svg></div></div><div class="w-full" style="margin-bottom: 0px;"><div class="w-full"><div class="text-token-text-primary text-[14px] leading-5">Thought for 9m 10s</div></div><div class="QKycbG_markdown text-token-text-secondary text-[14px] leading-5 markdown prose dark:prose-invert w-full break-words light markdown-new-styling"><p data-start="0" data-end="4" data-is-last-node="" data-is-only-node="">Done</p></div></div></div></div></div></div></section>'''
+regex = r'''( ?[A-Za-z\d_-]+=["'][A-Za-z\-_\.,\d\[\]\(\) ;:\/\/]*["'] ?)'''
+result = re.sub(regex, '', html)
 print(result)
