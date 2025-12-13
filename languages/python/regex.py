011f33dcb448b3ab06da59e89ef11f3748968d40
Chris Carl <chrisbcarl@outlook.com>
2025-12-13 03:15:52 -0800

    html regex at it again

--- languages/python/regex.py
+++ languages/python/regex.py
@@ -59,7 +59,9 @@ print(result)
 # takes 2 regexes
 print('\n\n\nex) 8: remove HTML tag attributes')  # <p _data-start="hello_world:0.0; 0.1-0.3,0.4">
 html = '''<path fill-rule="evenodd" clip-rule="evenodd" d="M10.3333 2.08496C14.7046 2.08496 18.2483 5.62867 18.2483 10C18.2483 14.3713 14.7046 17.915 10.3333 17.915C5.96192 17.915 2.41821 14.3713 2.41821 10C2.41821 5.62867 5.96192 2.08496 10.3333 2.08496ZM10.3333 3.41504C6.69646 3.41504 3.74829 6.3632 3.74829 10C3.74829 13.6368 6.69646 16.585 10.3333 16.585C13.97 16.585 16.9182 13.6368 16.9182 10C16.9182 6.3632 13.97 3.41504 10.3333 3.41504Z"></path></svg></div></div><div class="w-full" style="margin-bottom: 0px;"><div class="w-full"><div class="text-token-text-primary text-[14px] leading-5">Thought for 9m 10s</div></div><div class="QKycbG_markdown text-token-text-secondary text-[14px] leading-5 markdown prose dark:prose-invert w-full break-words light markdown-new-styling"><p data-start="0" data-end="4" data-is-last-node="" data-is-only-node="">Done</p></div></div></div></div></div></div></section>'''
-regex = r'''( ?[A-Za-z\d_-]+=["'][A-Za-z\-_\.,\d\[\]\(\) ;:\/\/]*["'] ?)'''
+# regex = r'''( ?[A-Za-z\d_-]+=["'][A-Za-z\-_\.,\d\[\]\(\) ;:\/\/]*["'] ?)'''
+# this one is simpler I think, and avoids href/src matching, or you could jigger it so it only finds href n src
+regex = r'''\s*(?P<key>(?:(?!href|src)\b)[A-Za-z\d_-]+)\s*=\s*["'](?P<value>.*?)["']\s*'''
 result = re.sub(regex, '', html)
 print(result)
 
@@ -71,6 +73,5 @@ Here we have a "quote" by a "great and respected author":
 Honestly it's kind of strange how we keep getting suckered into reading about him.
 Weird quote sticking out " without anybody to "capture" it--WARNING: WILL CAUSE PROBLEMS.
 '''
-regex = r'''( ?[A-Za-z\d_-]+=["'][A-Za-z\-_\.,\d\[\]\(\) ;:\/\/]*["'] ?)'''
 result = re.sub(r'"(.*?)"', r"``\1''", text)
 print(result)
