32bfc1c26abba339f35b7edbf0ba8b0fa3c6c644
Chris Carl <chrisbcarl@outlook.com>
2026-01-04 21:10:12 -0800

    LF configured with git

--- scripts/git-all.ps1
+++ scripts/git-all.ps1
@@ -1,9 +1,18 @@
 $count = 0
-$ArgsCopy = "$args"
+$ArgsCopy = $args
 Get-ChildItem | ForEach-Object {
     if (Test-Path -Path "$($_.FullName)/.git" -ErrorAction SilentlyContinue) {
         Write-Host -ForegroundColor Green $_;
-        git -C $_ $ArgsCopy
+        $FullArgs = @("-C", $_) + $ArgsCopy
+        $cmd = "git $FullArgs"
+        Write-Host -ForegroundColor Cyan $cmd
+        # Write-Host $FullArgs
+        & git @FullArgs
+        # Start-Process -FilePath 'git' -ArgumentList $FullArgs -Wait
+        if ($LASTEXITCODE -ne 0) {
+            Write-Warning "FAILED: $cmd"
+            exit $LASTEXITCODE
+        }
         $count += 1
     }
 }
