c83588914fb4d5275a5dea24df4aaa8e80a58750
Chris Carl <chrisbcarl@outlook.com>
2026-01-31 20:15:33 -0800

    expounded on install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+# run this from any new vm instance
+
+sudo sh -c "yes | unminimize"
+sudo apt install unzip -y
+
+mkdir ~/downloads
+curl -o ~/downloads/bootstrap-main.zip -L https://github.com/chrisbcarl/bootstrap/archive/refs/heads/main.zip  # -o output file, -L follow the redirects
+unzip ~/downloads/bootstrap-main.zip -d ~/downloads  # since its one folder nested, boostrap-main will just reveal itself
+
+chmod +x ~/downloads/bootstrap-main/operating-systems/posix/ubuntu-on-new-pc.sh
+sudo ~/downloads/bootstrap-main/operating-systems/posix/ubuntu-on-new-pc.sh
+
+# TODO: elaborate this section like .ps1
 poetry config virtualenvs.create true
 poetry config virtualenvs.in-project true
 poetry install
