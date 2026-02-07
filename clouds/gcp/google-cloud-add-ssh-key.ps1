868842485f756997b2317bb3de4b233a8638461c
Chris Carl <chrisbcarl@outlook.com>
2026-02-07 02:10:39 -0800

    ssh key add to gcp

---
+++ clouds/gcp/google-cloud-add-ssh-key.ps1
@@ -0,0 +1,2 @@
+
+gcloud compute os-login ssh-keys add --key-file (Resolve-Path ~/.ssh/id_ed25519.pub).Path --ttl "999d"
---
+++ scripts/gcp-add-ssh-key.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell %~dp0..\clouds\gcp\google-cloud-add-ssh-key.ps1 %*
+@echo on
