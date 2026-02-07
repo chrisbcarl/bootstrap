504e3e50d561ba41920e2675b767de30c8f34195
Chris Carl <chrisbcarl@outlook.com>
2026-02-07 02:22:29 -0800

    much more full add ssh key

--- clouds/gcp/google-cloud-add-ssh-key.ps1
+++ clouds/gcp/google-cloud-add-ssh-key.ps1
@@ -1,2 +1,16 @@
+$ssh_dirpath = (Resolve-Path ~/.ssh).Path
+$items = (Get-ChildItem -Path $ssh_dirpath)
+$ssh_pub_key = $null
+foreach ($item in $items) {
+    if (([IO.Path]::GetExtension($item.Name)) -eq '.pub') {
+        $ssh_pub_key = [IO.Path]::GetFullPath("$ssh_dirpath\$item")
+        Write-Host $ssh_pub_key
+        break
+    }
+}
 
-gcloud compute os-login ssh-keys add --key-file (Resolve-Path ~/.ssh/id_ed25519.pub).Path --ttl "999d"
+if ($null -ne $ssh_pub_key) {
+    gcloud compute os-login ssh-keys add --key-file $ssh_pub_key --ttl "999d"
+} else {
+    Write-Warning "Could not determine .pub keyfile from '$ssh_dirpath'!"
+}
